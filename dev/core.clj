67b5b09133604c6f8cad3cec751bf42ef51d542f
Stephen Kim <stephen422@gmail.com>
2015-04-30 23:06:41 +0900

    Setup dev env

---
+++ dev/core.clj
@@ -0,0 +1,3 @@
+(ns globe.core
+  (use [clojure.repl :refer :all]
+       [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject globe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Minecraft world file wrangler"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [clojurewerkz/buffy "1.0.1"]]
   :main ^:skip-aot globe.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.9"]]}
+             :uberjar {:aot :all}})
